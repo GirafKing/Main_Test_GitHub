0c1625529bdbf8e84a336fd72361a07be420142b
GirafKing <kuro0030@naver.com>
2025-09-18 15:17:33 +0900

    정답 코드

--- BJ_18242.py
+++ BJ_18242.py
@@ -3,6 +3,8 @@ count = 0
 result = 0
 boolean = True
 boolean_2 = False
+start = -1
+end = 0
 
 for i in range(N) :
     line = list(input())
@@ -10,25 +12,33 @@ for i in range(N) :
     if line.count("#") >= 3 and boolean :
         count = line.count("#")
         boolean = False
+
     elif line.count("#") >= 3 and boolean == False :
         if count < line.count("#") :
             result = "UP"
             boolean_2 = True
+
         elif count > line.count("#") :
             result = "DOWN"
             boolean_2 = True
+
         else :
             continue
 
-    elif line.count("#") >= 1 :
-        if line.count("#") != 2 :
-            line_left = line[:len(line) // 2]
-            line_right = line[len(line) // 2:]
-            if "#" in line_left :
-                result = "RIGHT"
-            else :
-                result = "LEFT"
-            
+    elif line.count("#") == 2 :
+        for j in range(len(line)) :
+            if line[j] == "#" :
+                if start == -1 :
+                    start = j
+                else :
+                    end = j
+
+    elif line.count("#") == 1 :
+        if line.index("#") == start :
+            result = "RIGHT"
+        elif line.index("#") == end :
+            result = "LEFT"
+
     else :
         continue
 
