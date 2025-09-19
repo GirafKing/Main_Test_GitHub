05ed7d619bc0115dd5795e148a35141885a90808
GirafKing <kuro0030@naver.com>
2025-09-20 06:10:37 +0900

    정답 코드

--- BJ_31799.py
+++ BJ_31799.py
@@ -3,17 +3,21 @@ N = int(input())
 scores = input()
 score_list = []
 
+grade = ""
 for i in scores :
     if i.isalpha() :
+        if grade :
+            score_list.append(grade)
         grade = i
     else :
         grade = grade + i
-        score_list.append(grade)
 
-for s in scores:
-    if len(s) == 1 :
-        s = s + "0" 
-    score_list.append(s)
+if grade:
+    score_list.append(grade)
+
+for i in range(len(score_list)):
+    if len(score_list[i]) == 1:
+        score_list[i] = score_list[i] + "0"
 
 result = ""
 previous_term = ""
@@ -25,32 +29,45 @@ for j in range(len(score_list)) :
         previous_term = score_list[j]
         term += 1
 
+
     elif score_list[j] in ["B0", "B-"] :
         if term == 1 or previous_term in ["C+", "C0", "C-"] :
             result += "D"
+            previous_term = score_list[j]
             term += 1
 
-    elif previous_term in ["A+", "A0", "A-", "B+", "B0", "B-"] :
-        result += "B"
-        term += 1
+        elif previous_term in ["A+", "A0", "A-", "B+", "B0", "B-"] :
+            result += "B"
+            previous_term = score_list[j]
+            term += 1
+
 
     elif score_list[j] in ["A-", "B+"] :
-            
         if term == 1 or previous_term in ["B0", "B-", "C+", "C0", "C-"] :
             result += "P"
+            previous_term = score_list[j]
             term += 1
 
-        elif previous_term in ["A", "A0", "A-", "B+"] :
+        elif previous_term in ["A+", "A0", "A-", "B+"] :
             result += "D"
+            previous_term = score_list[j]
             term += 1
+
 
     elif score_list[j] == "A0" :
-
         if term == 1 or previous_term in ["A-", "B+", "B0", "B-", "C+","C0", "C-"] :
+            result += "E"
+            previous_term = score_list[j]
+            term += 1
+        elif previous_term in ["A+", "A0"] :
             result += "P"
+            previous_term = score_list[j]
             term += 1
     
+
     elif score_list[j] == "A+" :
         result += "E"
+        previous_term = score_list[j]
+        term +=1
 
 print(result)
