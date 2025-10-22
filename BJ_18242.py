8635455da2b6aa0d1086958c08003d1582d65cb7
GirafKing <kuro0030@naver.com>
2025-10-22 09:11:21 +0900

    변경

--- BJ_18242.py
+++ BJ_18242.py
@@ -1,26 +1,22 @@
 N, M = map(int, input().split())
 count = 0
-result = 0
-boolean = True
-boolean_2 = False
+result = ""
 start = -1
 end = 0
 
 for i in range(N) :
     line = list(input())
 
-    if line.count("#") >= 3 and boolean :
-        count = line.count("#")
-        boolean = False
+    if line.count("#") >= 3:
 
-    elif line.count("#") >= 3 and boolean == False :
-        if count < line.count("#") :
+        if count == 0:
+            count = line.count("#")
+
+        elif count < line.count("#"):
             result = "UP"
-            boolean_2 = True
-
-        elif count > line.count("#") :
+            
+        elif count > line.count("#"):
             result = "DOWN"
-            boolean_2 = True
 
         else :
             continue
