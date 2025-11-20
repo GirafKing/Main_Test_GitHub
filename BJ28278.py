aae26f08eea1ca1b92d6895099750fd3fb64e0ba
GirafKing <kuro0030@naver.com>
2025-11-20 10:52:02 +0900

    2025.11.20 두 번째

--- BJ28278.py
+++ BJ28278.py
@@ -1,28 +1,31 @@
-N = int(input())
+out = []
 stack = []
+
+N = int(input())
+
 for _ in range(N) :
     cmd = input().split()
+
     if cmd[0] == '1' :
         x = int(cmd[1])
         stack.append(x)
 
     elif cmd[0] == '2' :
         if stack :
-            print(stack[0])
+            out.append(str(stack.pop()))
         else :
-            print(-1)
+            out.append('-1')
     
     elif cmd[0] == '3' :
-        print(len(stack))
+        out.append(str(len(stack)))
 
     elif cmd[0] == '4' :
-        if not stack :
-            print(1)
-        else :
-            print(0)
+        out.append('1' if not stack else '0')
 
     elif cmd[0] == '5' :
         if stack :
-            print(stack[0])
+            out.append(str(stack[-1]))
         else :
-            print(-1)
+            out.append('-1')
+
+print('\n'.join(out))
