001997ef1dfde95c42dbd927de9fc6b724f92af9
GirafKing <kuro0030@naver.com>
2025-11-29 09:58:09 +0900

    first

---
+++ BJ2346.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+N = int(input())
+papers = list(map(int, input().split()))
+out = []
+
+dq = deque()
+for i in range(N) :
+    dq.append((i+1, papers[i]))
+
+for _ in range(N) :
+    num, paper = dq.popleft()
+    out.append(str(num))
+    if paper > 0 :      
+        dq.rotate(paper - 1)
+    else :
+        dq.rotate(paper + 1)
+
+print(' '.join(out))
