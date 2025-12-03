c39751e03b662b2657000437f2a37f74b3fcd7c0
GirafKing <kuro0030@naver.com>
2025-12-03 09:28:13 +0900

    AI thinking

--- BJ13909.py
+++ BJ13909.py
@@ -1,18 +1,8 @@
 import sys
+import math
+
 input = sys.stdin.readline
 
 N = int(input())
 
-windows = [0 for i in range(1, N+1)]
-print(windows)
-
-for i in range(1, N+1) :
-    for j in range(1, N+1) :
-        if (j % i) == 0 :
-            if windows[j-1] == 0 :
-                windows[j-1] = 1
-            else :
-                windows[j-1] = 0
-    print(windows)
-
-print(windows.count(1))
+print(int(math.isqrt(N)))
