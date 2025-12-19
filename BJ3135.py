9ad6ec2b9c6b95d2c52797c41f83b38c9a7c978c
GirafKing <kuro0030@naver.com>
2025-12-19 19:56:32 +0900

    수정 중

--- BJ3135.py
+++ BJ3135.py
@@ -9,3 +9,11 @@ for _ in range(N) :
 current_hz = A
 count = 0
 
+close_radio_button = min(dict, key=dict.get)
+if abs(A - B) > dict[close_radio_button] :
+    current_hz = close_radio_button
+    count += 1
+else :
+    count += abs(current_hz - B) + 1
+
+print(count)
