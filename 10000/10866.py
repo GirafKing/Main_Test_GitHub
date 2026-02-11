f0dfd97bfe6653204733996e096a2ff0697db33b
GirafKing <kuro0030@naver.com>
2026-02-11 16:04:10 +0900

    Answer Code

--- 10000/10866.py
+++ 10000/10866.py
@@ -1,41 +1,49 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
+
 
 N = int(input())
 
 q = deque()
 for _ in range(N) :
-    a = input()
+    a = input().split()
     
     if a[0] == "push_front" :
         q.appendleft(int(a[1]))
+
     elif a[0] == "push_back" :
         q.append(int(a[1]))
+
     elif a[0] == "pop_front" :
         if q :
             print(q.popleft())
         else :
             print(-1)
+
     elif a[0] == "pop_back" :
         if q :
             print(q.pop())
         else :
             print(-1)
+
     elif a[0] == "size" :
         print(len(q))
+
     elif a[0] == "empty" :
         if q :
             print(0)
         else :
             print(1)
+
     elif a[0] == "front" :
         if q :
-            print(q.popleft())
-        else :
-            print(-1)
-    elif a[0] == "back" :
-        if q :
-            print(q.pop())
+            print(q[0])
         else :
             print(-1)
 
-    print(q)
+    elif a[0] == "back" :
+        if q :
+            print(q[-1])
+        else :
+            print(-1)
